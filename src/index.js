c728284fae5afca7c4d7e8cbe0bcb9f9a68436b6
Alex Clark <aclark@aclark.net>
2021-08-18 15:14:17 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './cover.css';
 
 // https://stackoverflow.com/a/53580347/185820
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/fontawesome'
+// import '@fortawesome/fontawesome-free/js/solid'
+// import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
