b58e2f07e9c759f1b27e18c12a2710911952c440
Alex Clark <aclark@aclark.net>
2021-08-18 14:55:18 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './cover.css';
+
+// https://stackoverflow.com/a/53580347/185820
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
