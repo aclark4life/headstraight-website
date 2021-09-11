7b93f0cab165f38a07c84aa5da4b36c5bff30a6c
Alex Clark <aclark@aclark.net>
2021-09-11 08:49:41 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import './cover.css';
 import '@fortawesome/fontawesome-free/js/fontawesome'
 // import '@fortawesome/fontawesome-free/js/solid'
 // import '@fortawesome/fontawesome-free/js/regular'
-// import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/brands'
