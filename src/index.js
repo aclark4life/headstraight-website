f16cdc4230e1a149ad1390ddd4fb64c4d04256d3
Alex Clark <aclark@aclark.net>
2022-01-27 09:47:05 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import './toolkit-bold.css';
 
 // https://stackoverflow.com/a/53580347/185820
 import '@fortawesome/fontawesome-free/js/fontawesome'
-// import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/solid'
 // import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
